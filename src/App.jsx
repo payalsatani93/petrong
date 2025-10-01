c005cae815b1215cfe37741413f485f1a6dbfcfc
payalsatani93 <payalsatani99@gmail.com>
2025-10-01 16:57:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Hero from './Home/components/Hero'
+import './App.css'
+import About from './components/About'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="">
+        <Hero />
+        <About/>
+      </div>
+    </>
+  )
+}
+
+export default App
