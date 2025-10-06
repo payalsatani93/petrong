efb493a134cfaf687dbd64204eff82fe7036ece4
payalsatani93 <payalsatani99@gmail.com>
2025-10-06 18:10:33 +0530

    add new change

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -1,6 +1,3 @@
-import React, { useState } from "react";
-
-
 import React from "react";
 
 export default function Hero() {
--- src/pages/Home/OurPromise.jsx
+++ src/pages/Home/OurPromise.jsx
@@ -9,53 +9,45 @@ import {
 } from "lucide-react";
 
 export default function OurPromise() {
-  const promises = [
-    {
-      icon: <Codesandbox className="w-8 h-8 text-gray-600" />,
-      title: "Top Code Quality",
-      description:
-        "What we promise is not simply code but rather a solution. You get a W3C fully compliant markup with no compromise on quality.",
-    },
-    {
-      icon: <MonitorSmartphone className="w-8 h-8 text-gray-600" />,
-      icon: <MonitorSmartphone className="w-8 h-8 text-gray-600" />,
-      title: "Responsive",
-      description:
-        "We know you look for positive attitude, timely reactions, and flexibility. We are here to listen and give the utmost of our capabilities.",
-    },
-    {
-      icon: <Users className="w-8 h-8 text-gray-600" />,
-      icon: <Users className="w-8 h-8 text-gray-600" />,
-      title: "Rockstar Team",
-      description:
-        "Expertise is our USP. You get an all-star squad of experienced geeks, fully at your service and armed with coding skills to die for.",
-    },
-    {
-      icon: <Undo2 className="w-8 h-8 text-gray-600" />,
-      icon: <Undo2 className="w-8 h-8 text-gray-600" />,
-      title: "Fast Turn-arounds",
-      description:
-        "Speed matters no less than quality. We are fast, agile and capable of delivering top-notch code in a timeframe chosen by you.",
-        "Speed matters no less than quality. We are fast, agile and capable of delivering top-notch code in a timeframe chosen by you.",
-    },
-    {
-      icon: <LifeBuoy className="w-8 h-8 text-gray-600" />,
-      title: "Life-time Support",
-      icon: <LifeBuoy className="w-8 h-8 text-gray-600" />,
-      title: "Life-time Support",
-      description:
-        "The ultimate proof of our code quality: you get our non-expiring support. If any bugs appear in the future, just write us a line.",
-        "The ultimate proof of our code quality: you get our non-expiring support. If any bugs appear in the future, just write us a line.",
-    },
-    {
-      icon: <ShieldCheck className="w-8 h-8 text-gray-600" />,
-      icon: <ShieldCheck className="w-8 h-8 text-gray-600" />,
-      title: "Secured Agreement",
-      description:
-        "Your artwork is fully yours and we would never claim otherwise. You get our word and our full willingness to sign an NDA.",
-        "Your artwork is fully yours and we would never claim otherwise. You get our word and our full willingness to sign an NDA.",
-    },
-  ];
+ const promises = [
+  {
+    icon: <Codesandbox className="w-8 h-8 text-gray-600" />,
+    title: "Top Code Quality",
+    description:
+      "What we promise is not simply code but rather a solution. You get a W3C fully compliant markup with no compromise on quality.",
+  },
+  {
+    icon: <MonitorSmartphone className="w-8 h-8 text-gray-600" />,
+    title: "Responsive",
+    description:
+      "We know you look for positive attitude, timely reactions, and flexibility. We are here to listen and give the utmost of our capabilities.",
+  },
+  {
+    icon: <Users className="w-8 h-8 text-gray-600" />,
+    title: "Rockstar Team",
+    description:
+      "Expertise is our USP. You get an all-star squad of experienced geeks, fully at your service and armed with coding skills to die for.",
+  },
+  {
+    icon: <Undo2 className="w-8 h-8 text-gray-600" />,
+    title: "Fast Turn-arounds",
+    description:
+      "Speed matters no less than quality. We are fast, agile and capable of delivering top-notch code in a timeframe chosen by you.",
+  },
+  {
+    icon: <LifeBuoy className="w-8 h-8 text-gray-600" />,
+    title: "Life-time Support",
+    description:
+      "The ultimate proof of our code quality: you get our non-expiring support. If any bugs appear in the future, just write us a line.",
+  },
+  {
+    icon: <ShieldCheck className="w-8 h-8 text-gray-600" />,
+    title: "Secured Agreement",
+    description:
+      "Your artwork is fully yours and we would never claim otherwise. You get our word and our full willingness to sign an NDA.",
+  },
+];
+
 
   return (
     <section className="bg-white py-16 px-6 lg:px-20">
@@ -67,7 +59,6 @@ export default function OurPromise() {
           As part of our high quality service, we'd like to offer something
           extra too.
         </p>
-      </div>
       </div>
 
       {/* Cards Grid */}
